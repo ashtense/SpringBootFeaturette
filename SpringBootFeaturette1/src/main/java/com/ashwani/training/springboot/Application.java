fca96e87dc90b5ca1184556041a13bc7975ca1b7
Ashwani Solanki <solanki.ashwani@yahoo.com>
2017-03-21 01:50:35 +0530

    SpringBoot - Controller configured through automatic configuration

--- SpringBootFeaturette1/src/main/java/com/ashwani/training/springboot/Application.java
+++ SpringBootFeaturette1/src/main/java/com/ashwani/training/springboot/Application.java
@@ -21,6 +21,7 @@ import org.springframework.stereotype.Controller;
 public class Application {
 
 	public static void main(String[] args) {
+		System.err.println("Ashtense - Geronimo");
 		SpringApplication.run(Application.class, args);
 	}
 }
--- SpringBootFeaturette1/src/main/java/com/ashwani/training/springboot/web/CustomerController.java
+++ SpringBootFeaturette1/src/main/java/com/ashwani/training/springboot/web/CustomerController.java
@@ -1,5 +1,19 @@
 package com.ashwani.training.springboot.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
 
+	@RequestMapping("/")
+	String home() {
+		return "Welcome - Regards, Ashtense";
+	}
+	
+	@RequestMapping("/ashtense")
+	String ashtense(){
+		return "Dude you're fast";
+	}
+	
 }
